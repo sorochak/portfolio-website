77c05214f80fefc3262fca337f3cd39200f80443
Austen Sorochak <austensorochak@gmail.com>
2023-08-27 16:06:26 -0700

    update structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 import './App.css';
 
@@ -10,12 +10,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        {/* Uncomment when you add these pages */}
+        {/* <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} /> */}
+      </Routes>
     </Router>
   );
 }
