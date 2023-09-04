8289283e755df588b43240cf13f2b698761e9bd3
Austen Sorochak <austensorochak@gmail.com>
2023-09-03 19:55:13 -0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,61 @@
-import { Box, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Typography, Container, Grid } from "@mui/material";
+import Link from "@mui/material/Link";
+import { LinkedIn, GitHub } from "@mui/icons-material";
+import Logo from "./Logo";
 
 const Footer = () => {
-
   return (
-    <Box>
-      <Typography>Footer</Typography>
+    <Box component="footer">
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Box width={150} height={150}>
+              <Logo width="75px" height="75px" />
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Let's Chat!
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: info@austensorochak.com
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Me
+            </Typography>
+            <Link
+              href="https://www.linkedin.com/in/austensorochak/"
+              color="inherit"
+              target="_blank"
+            >
+              <LinkedIn />
+            </Link>
+            <Link
+              href="https://github.com/sorochak"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+              target="_blank"
+            >
+              <GitHub />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" component={RouterLink} to="/">
+              Austen Sorochak
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
     </Box>
-  )
-  
-}
+  );
+};
 
 export default Footer;
