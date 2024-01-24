ac1ee69a2e78749c89bc18f6a93d32d478a8d00c
Austen Sorochak <austensorochak@gmail.com>
2024-01-23 19:23:45 -0800

    update about me section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import { Box, Typography, Button, useTheme, useMediaQuery } from "@mui/material";
+import { Box, Typography, Button, useTheme, useMediaQuery, Container, Grid, Paper } from "@mui/material";
 import { useContext } from "react";
 import backgroundImage from "../static/nayuca.webp";
 import { ColorModeContext } from "../components/BaseView";
+import signature from "../static/sorochakSignature.png"
 
 const Home = () => {
   const theme = useTheme();
@@ -17,6 +18,7 @@ const Home = () => {
       : "1px 1px 20px #fff, 0 0 25px #fff, 0 0 15px #fff";
 
   return (
+    <>
     <Box
       component="div"
       sx={{
@@ -130,6 +132,38 @@ const Home = () => {
         </Box>
       </Box>
     </Box>
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Paper elevation={3} style={{ padding: '20px' }}>
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Typography variant="h4" gutterBottom>
+                Hi, I'm Austen!
+              </Typography>
+            </Box>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+            My vision is to help create an enlightened society...
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+            This begins with examining my own experiences to understand the value I bring to the world.
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+            Passionate about leveraging technology to better understand our surroundings, I aim to bring scientific data to the web in interactive and impactful ways, assisting in addressing the pressing challenges of the 21st century. From my roots as an Environmental Scientist, technology has always held a pivotal role in my endeavours.            
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+            Having graduated from Camosun College with a focus on Information & Computer Systems, I now work as a Full-Stack Software Developer. My core responsibility revolves around designing, developing, and deploying software solutions that efficiently collect, transform, visualize, and distribute scientific data. With expertise in tools such as JavaScript, Postgresql, Docker, and Git, I ensure seamless interactions for users with intricate datasets. My proficiency also extends to Python/Django and various Cloud Products.            
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+            My environmental management journey spans over seven years, touching various industries like mining, oil & gas, water treatment, and contaminated sites remediation. This profession has taken me across the vast expanses of Canada, from military training areas in the prairies to the icy shores of northern Baffin Island, Nunavut, and even remote indigenous communities on the central coast of British Columbia.          
+            </Typography>
+            <Box mt={4} display="flex" justifyContent="flex-end">
+              <img src={signature} alt="Austen's Signature" style={{ maxWidth: '150px' }} />
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+    </>
   );
 };
 
