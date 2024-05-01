018b43eae0627fab6769cc760fe613927f8982cf
Austen Sorochak <austensorochak@gmail.com>
2024-05-01 14:12:59 -0700

    style updates

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 
-const Logo = ({ size = "medium", width, height }) => {
+const Logo = ({ size = "medium", width, height, style }) => {
   const theme = useTheme();
 
   // Using theme's spacing to define standard sizes
@@ -16,26 +16,56 @@ const Logo = ({ size = "medium", width, height }) => {
   const finalHeight = height || sizes[size];
 
   // Adjust the stroke color based on the theme mode
-  const strokeColor = theme.palette.mode === 'dark' ? '#ffffff' : '#000000';
+  const strokeColor = theme.palette.mode === "dark" ? "#ffffff" : "#000000";
 
   return (
-    <svg viewBox="0 0 500 500" width={finalWidth} height={finalHeight} xmlSpace="preserve">
-    <desc>Created with Fabric.js 5.3.0</desc>
-    <g transform="matrix(-4.225 0 0 -4.225 181.3181164685 196.7164825095)">
-      <path style={{
-        stroke: strokeColor, strokeWidth: 5, strokeDasharray: "none", strokeLinecap: "butt",
-        strokeDashoffset: 0, strokeLinejoin: "miter", strokeMiterlimit: 4, fill: "none",
-        fillRule: "nonzero", opacity: 1,
-      }} transform=" translate(0, 0)" d="M 20 0 L 40 40 L 0 40 L -40 40 L -20 0 L 0 -40 z" strokeLinecap="round" />
-    </g>
-    <g transform="matrix(-4.229709207 0 0 -4.229709207 319.729942742 277.750054375)">
-      <path style={{
-        stroke: strokeColor, strokeWidth: 5, strokeDasharray: "none", strokeLinecap: "butt",
-        strokeDashoffset: 0, strokeLinejoin: "miter", strokeMiterlimit: 4, fill: "none",
-        fillRule: "nonzero", opacity: 1,
-      }} transform=" translate(0, 0)" d="M 20 0 L 40 40 L 0 40 L -40 40 L -20 0 L 0 -40 z" strokeLinecap="round" />
-    </g>
-  </svg>
+    <svg
+      viewBox="0 0 500 500"
+      width={finalWidth}
+      height={finalHeight}
+      style={style}
+      xmlSpace="preserve"
+    >
+      <desc>Created with Fabric.js 5.3.0</desc>
+      <g transform="matrix(-4.225 0 0 -4.225 181.3181164685 196.7164825095)">
+        <path
+          style={{
+            stroke: strokeColor,
+            strokeWidth: 5,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fill: "none",
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          transform=" translate(0, 0)"
+          d="M 20 0 L 40 40 L 0 40 L -40 40 L -20 0 L 0 -40 z"
+          strokeLinecap="round"
+        />
+      </g>
+      <g transform="matrix(-4.229709207 0 0 -4.229709207 319.729942742 277.750054375)">
+        <path
+          style={{
+            stroke: strokeColor,
+            strokeWidth: 5,
+            strokeDasharray: "none",
+            strokeLinecap: "butt",
+            strokeDashoffset: 0,
+            strokeLinejoin: "miter",
+            strokeMiterlimit: 4,
+            fill: "none",
+            fillRule: "nonzero",
+            opacity: 1,
+          }}
+          transform=" translate(0, 0)"
+          d="M 20 0 L 40 40 L 0 40 L -40 40 L -20 0 L 0 -40 z"
+          strokeLinecap="round"
+        />
+      </g>
+    </svg>
   );
 };
 
