41d86bf4cb799bcb4b1a86ed87b31b16d78aef27
Austen Sorochak <austensorochak@gmail.com>
2024-05-03 00:23:10 -0700

    remove contact page

--- src/components/BaseRouter.jsx
+++ src/components/BaseRouter.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
-import Contact from "../pages/Contact";
 
 const BaseRouter = () => {
   return (
@@ -9,7 +8,6 @@ const BaseRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<Home />} />
       <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
