7d1667837f0081eea1e95fc02f78cc161552f7eb
Austen Sorochak <austensorochak@gmail.com>
2024-05-06 23:29:22 -0700

    fix deploy to gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import "@fontsource/roboto/700.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
