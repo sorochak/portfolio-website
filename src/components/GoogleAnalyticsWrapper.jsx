2e45c77f072a88963b2bd6dc78b395e0e8f945c1
Austen Sorochak <austensorochak@gmail.com>
2024-05-12 18:22:25 -0700

    fix analytics

--- src/components/GoogleAnalyticsWrapper.jsx
+++ src/components/GoogleAnalyticsWrapper.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import useGoogleAnalytics from "../hooks/useGoogleAnalytics";
 
 const GoogleAnalyticsWrapper = ({ children }) => {
