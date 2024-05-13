5b4b9a67d648224a7b0ac3d0a2007cdbfa4f219a
Austen Sorochak <austensorochak@gmail.com>
2024-05-12 17:38:37 -0700

    add google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Box } from "@mui/material";
 import BaseView from "./components/BaseView";
+import useTracking from "./hooks/useTracking";
 import "./App.css";
 
+const TRACKING_ID = "G-RMZW4VZS2Z";
+
 function App() {
+  useTracking(TRACKING_ID);
   return (
     <Box>
       <BaseView />
