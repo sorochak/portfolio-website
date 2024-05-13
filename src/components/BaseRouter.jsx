2a2f5f64bb0b025e1653a9dad08d1c3ddb038e40
Austen Sorochak <austensorochak@gmail.com>
2024-05-12 18:14:11 -0700

    update analytics

--- src/components/BaseRouter.jsx
+++ src/components/BaseRouter.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Projects from "../pages/Projects";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
-import ReactGA from "react-ga";
-const TRACKING_ID = "G-XRX88BJN0C";
-
-ReactGA.initialize(TRACKING_ID);
 
 const BaseRouter = () => {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
