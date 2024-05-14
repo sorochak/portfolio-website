e3ece468235792cb1d4f10f2ff1ffa804ffd64da
Austen Sorochak <austensorochak@gmail.com>
2024-05-13 23:00:04 -0700

    add Sentry error tracking to client side contact submission

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,22 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2f011293e9d752869e3c3a76a5811477@o4507252884045824.ingest.us.sentry.io/4507252895383552",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/austensorochak\.com\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
